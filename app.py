ddbfab4f865f5f637db86b5ce9b433ced9628d7d
Siddharth Patankar <siddharthpatankar123@gmail.com>
2026-09-02 11:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,41 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <html>
-        <head>
-            <title>Kastro Python Application</title>
-        </head>
-
-        <body>
-            <h1>Hello from Kastro Python Application!</h1>
-
-            <h2>Application Information</h2>
-
-            <ul>
-                <li>Language: Python</li>
-                <li>Framework: Flask</li>
-                <li>Container: Docker</li>
-                <li>CI/CD: GitHub Actions</li>
-                <li>Runner: Self-hosted</li>
-            </ul>
-
-            <p>Application deployed successfully.</p>
-        </body>
-    </html>
-    """
+    return "Hello from Python Docker Application!"
 
 
 @app.route("/health")
 def health():
-    return {
-        "status": "UP",
-        "application": "kastro-python-app"
-    }
+    return "Application is UP"
 
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=8080
-    )
+    app.run(host="0.0.0.0", port=5000)
